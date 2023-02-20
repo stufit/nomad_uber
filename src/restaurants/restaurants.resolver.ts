5e3196afa85bef85ceb2e7af9577736ca18ca5bd
이관영 <stufit@marketboro.com>
2023-02-20 18:32:08 +0900

    2.3 Arguments

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,11 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver((of) => Restaurant)
 export class RestaurantsResolver {
-  @Query((returns) => Restaurant)
-  myRestaurant() {
-    return true;
+  @Query((returns) => [Restaurant])
+  restaurant(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    console.log(veganOnly);
+    return [];
   }
 }
