940d774e250ed0c1dacc7c073e741c72d64b0bc9
이관영 <stufit@marketboro.com>
2023-02-22 17:18:00 +0900

    3.4 Introducing ConfigService => dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,15 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     RestaurantsModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // 어플리케이션의 어디서나 config 모듈에 접근할 수 있음
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
