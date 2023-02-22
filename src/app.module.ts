1634761ccdede89ae6697392fbc5c782f10f094c
이관영 <stufit@marketboro.com>
2023-02-22 17:30:18 +0900

    3.5 configuring configService

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,14 +11,15 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true, // 어플리케이션의 어디서나 config 모듈에 접근할 수 있음
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod', // 서버에 배포시에는 환경변수 파일을 사용하지 않음
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'stufit',
-      password: 'stufit',
-      database: 'nuber-eats',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT, // + 를 붙이면 string으로 변환해준다.
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       synchronize: true,
       logging: true,
     }),
