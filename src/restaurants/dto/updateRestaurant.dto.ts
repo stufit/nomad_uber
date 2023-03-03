e1f1700d6294564f96e7c33a8c49ba7f7d2f6337
이관영 <stufit@marketboro.com>
2023-03-03 10:39:25 +0900

    4.7 Update Restaurant part One

---
+++ src/restaurants/dto/updateRestaurant.dto.ts
@@ -0,0 +1,15 @@
+import { ArgsType, Field, InputType, PartialType } from '@nestjs/graphql';
+import { CreateRestaurantInputDto } from './createRestaurantInput.dto';
+
+@InputType()
+export class UpdateRestaurantInputType extends PartialType(
+  CreateRestaurantInputDto,
+) {}
+
+@InputType()
+export class UpdateRestaurantDto {
+  @Field((type) => Number)
+  id: number;
+  @Field((type) => UpdateRestaurantInputType)
+  data: UpdateRestaurantInputType;
+}
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -2,14 +2,17 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 import { CreateRestaurantInputDto } from './dto/createRestaurantInput.dto';
 import { RestaurantService } from './restaurant.service';
+import { UpdateRestaurantDto } from './dto/updateRestaurant.dto';
 
 @Resolver((of) => Restaurant)
 export class RestaurantsResolver {
   constructor(private readonly restaurantService: RestaurantService) {}
+
   @Query((returns) => [Restaurant])
   restaurant(): Promise<Restaurant[]> {
     return this.restaurantService.getAll();
   }
+
   @Mutation((returns) => Boolean)
   async createRestaurant(
     @Args('input')
@@ -24,5 +27,12 @@ export class RestaurantsResolver {
       console.log(e);
       return false;
     }
+  }
+
+  @Mutation((returns) => Boolean)
+  async updateRestaurant(
+    @Args('data') updateRestaurantDto: UpdateRestaurantDto,
+  ) {
+    return true;
   }
 }
