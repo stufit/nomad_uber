7efdd97c849544b1ea0202a181b77109986c1982
이관영 <stufit@marketboro.com>
2023-03-03 14:36:34 +0900

    5.3 Create Account Mutation part One

--- src/uesrs/users.resolver.ts
+++ src/uesrs/users.resolver.ts
@@ -1,8 +1,15 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dto/createAccount.dto';
 
 @Resolver((of) => User)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
+
+  @Mutation((returns) => CreateAccountOutput)
+  createAccount(@Args('input') createAccountInput: CreateAccountInput) {}
 }
