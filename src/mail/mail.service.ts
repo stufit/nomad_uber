a053fc04c36c1e539652f417c72697e17d21358f
이관영 <stufit@marketboro.com>
2023-03-17 16:22:17 +0900

    7.7 Mailgun API

---
+++ src/mail/mail.service.ts
@@ -0,0 +1,36 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CONFIG_OPTIONS } from '../common/common.constants';
+import got from 'got';
+import * as FormData from 'form-data';
+import { MailModuleOptions } from './mail.interfaces';
+
+@Injectable()
+export class MailService {
+  constructor(
+    @Inject(CONFIG_OPTIONS)
+    private readonly options: MailModuleOptions,
+  ) {
+    this.sendEmailService('테스트', '테스틍');
+    console.log(options);
+  }
+  private async sendEmailService(subject: string, content: string) {
+    const form = new FormData();
+    form.append('from', `Excited User <mailgun@${this.options.domain}>`);
+    form.append('to', `rhksdud23000@naver.com`);
+    form.append('subject', subject);
+    form.append('text', content);
+    const response = await got(
+      `https://api.mailgun.net/v3/${this.options.domain}/messages`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `api:${this.options.apiKey}`,
+          ).toString('base64')}`,
+        },
+        body: form,
+      },
+    );
+    console.log(response.body);
+  }
+}
